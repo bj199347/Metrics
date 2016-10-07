358291939e76fcae8b16897895beb9f714af7ae5
Martin Bell <martbell79@gmail.com>
2016-10-07 22:58:10 +0100

    Added sports back office api

---
+++ OwinMetricsConsole/SportsBackOfficeController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web;
+using System.Web.Hosting;
+using System.Reflection;
+using System;
+using System.Web.Http.Description;
+
+namespace  AspNetSelfHostDemo
+{
+    public class SportsBackOfficeController:ApiController
+    {
+        [Route("{*actionName}")]
+        [AcceptVerbs("GET", "POST", "PUT", "DELETE")]//Include what ever methods you want to handle
+        [AllowAnonymous]//So I can use it on authenticated controllers
+        [ApiExplorerSettings(IgnoreApi = true)]//To hide this method from helpers
+        public virtual HttpResponseMessage HandleUnknownAction(string actionName)
+        {
+            var response = new HttpResponseMessage();
+            
+            response.Content = new StringContent("Get Started with the Sports Back Office API");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+
+        [Route("{format}/fixture/{fixtureid}")]
+        public HttpResponseMessage GetFixtureDetailsById(string format, int fixtureid)
+        {
+            // Call database to get the Fixture Details By Id
+
+            // Serialize to JSON string
+            string json = "{fixtureid:"+fixtureid.ToString()+"}";
+
+            return ConstructResponse(format, json, "Fixture Details:" + fixtureid.ToString());
+        }
+
+        private HttpResponseMessage ConstructResponse(string format, string json, string htmlPageTitle)
+        {
+            var response = new HttpResponseMessage();
+
+            if (format.ToLower() == "json")
+            {
+                response.Content = new StringContent(json);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/json");
+            }
+            else if (format.ToLower() == "html")
+            {
+                response.Content = new StringContent(GetHtmlPageText(json, htmlPageTitle));
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            }
+            else
+            {
+                response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                response.Content = new StringContent("Invalid response type requested in the request URL.  Options are json and html.");
+            }
+
+            return response;
+        }
+
+        private string GetHtmlPageText(string json, string pageTitle)
+        {
+            string pageText = "";
+            var path = HostingEnvironment.MapPath("~/HTML/JsonFormatter.html");
+            if (path == null)
+            {
+                var uriPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+                path = new Uri(uriPath).LocalPath + "/HTML/JsonFormatter.html";
+            }
+            pageText = File.ReadAllText(path);
+            return pageText.Replace("{{JSON}}", json).Replace("{{TITLE}}", pageTitle);
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string html = "<html><head></head><body style='background-color: grey; '>Some body text</body></html>";
+
+            webBrowser1.Navigate("about:blank");
+            while (webBrowser1.Document == null || webBrowser1.Document.Body == null)
+                Application.DoEvents();
+            webBrowser1.Document.OpenNew(true).Write(html);
+        }
+    }
+}
